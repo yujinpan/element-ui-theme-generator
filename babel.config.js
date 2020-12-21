e6ccd922edde77f410646329336572bb02dfd9a0
yujinpan <1192878390@qq.com>
2020-12-21 09:48:38 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
       {
         libraryName: 'element-ui',
         styleLibrary: {
-          name: '~src/lib',
+          name: '~lib', // 主题根目录
           path: '[module].css',
           base: true
         }
